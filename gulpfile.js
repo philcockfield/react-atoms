f5a19df6d0fb1499fdb7b88b5bfecd47400fefcf
Phil Cockfield <phil@cockfield.net>
2015-09-10 12:43:09 +1200

    Move /components => /src/components

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,9 @@
 "use strict"
 var gulp = require("gulp");
 var eslint = require("gulp-eslint");
-// var babel = require("gulp-babel");
-var SOURCE_PATH = ["./components/**/*.js", "./components/**/*.jsx"];
+var babel = require("gulp-babel");
 
-
+var SOURCE_PATH = ["./src/**/*.js", "./src/**/*.jsx"];
 
 gulp.task("lint", function() {
   return gulp.src(SOURCE_PATH)
@@ -13,5 +12,12 @@ gulp.task("lint", function() {
 });
 
 
+gulp.task("build", function() {
+  return gulp.src(SOURCE_PATH)
+    .pipe(babel({ stage: 1 }))
+    .pipe(gulp.dest("./"));
+});
 
-gulp.task("default", ["lint"]);
+gulp.task("prepublish", ["build"]);
+gulp.task("watch", function(callback) { gulp.watch("./src/**/*", ["build"]) });
+gulp.task("default", ["build", "watch"]);
