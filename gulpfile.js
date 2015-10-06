ded7b942bd089abf5b858b2f67e0eeeb3d05585a
Phil Cockfield <phil@cockfield.net>
2015-10-05 19:13:03 -0700

    Ensure build tasks stay alive.

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,15 @@
 "use strict"
 var gulp = require("gulp");
+var plumber = require('gulp-plumber');
 var eslint = require("gulp-eslint");
 var babel = require("gulp-babel");
-
 var SOURCE_PATH = ["./src/**/*.js", "./src/**/*.jsx"];
 
 gulp.task("lint", function() {
   return gulp.src(SOURCE_PATH)
-    .pipe(eslint())
-    .pipe(eslint.format());
+             .pipe(plumber()) // Keep task alive on build errors.
+             .pipe(eslint())
+             .pipe(eslint.format());
 });
 
 gulp.task("build", function() {
