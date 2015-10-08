84c01f25467ffbf7346a9790539986eb6b6d6c6f
Phil Cockfield <phil@cockfield.net>
2015-10-08 16:43:28 -0700

    <Twisty>: Remove `verticalAlign` property.

--- src/components/Twisty.jsx
+++ src/components/Twisty.jsx
@@ -10,30 +10,25 @@ import { css, PropTypes } from "js-util/react";
 @Radium
 export default class Twisty extends React.Component {
   styles() {
-    let { width, color, opacity, isOpen, isAnimated, margin, verticalAlign } = this.props;
+    let { width, color, opacity, isOpen, isAnimated, margin } = this.props;
     const narrow = Math.round(width * 0.666666)
     if (opacity < 0) { opacity = 0; }
     if (opacity > 1) { opacity = 1; }
-
-    const base = {
-      display: "inline-block",
-      width: 0,
-      height: 0,
-      opacity: opacity,
-      borderTop: `${ narrow }px solid transparent`,
-      borderBottom: `${ narrow }px solid transparent`,
-      borderLeft: `${ width }px solid ${ color }`,
-      transform: `rotate(${ isOpen ? "90deg" : "0deg" })`,
-      transition: isAnimated ? "transform 0.15s" : "none",
-      cursor: "pointer",
-      margin: margin
-    };
-
-    if (verticalAlign) {
-      base.AbsoluteCenter = "y";
-    }
-
-    return css({ base });
+    return css({
+      base: {
+        display: "inline-block",
+        width: 0,
+        height: 0,
+        opacity: opacity,
+        borderTop: `${ narrow }px solid transparent`,
+        borderBottom: `${ narrow }px solid transparent`,
+        borderLeft: `${ width }px solid ${ color }`,
+        transform: `rotate(${ isOpen ? "90deg" : "0deg" })`,
+        transition: isAnimated ? "transform 0.15s" : "none",
+        cursor: "pointer",
+        margin: margin
+      }
+    });
   }
 
   handleClick(e) {
@@ -57,8 +52,7 @@ Twisty.propTypes = {
   width: PropTypes.number,
   color: PropTypes.string,
   opacity: PropTypes.number,
-  margin: PropTypes.numberOrString,
-  verticalAlign: PropTypes.bool
+  margin: PropTypes.numberOrString
 };
 Twisty.defaultProps = {
   isAnimated: true,
@@ -66,6 +60,5 @@ Twisty.defaultProps = {
   width: 8,
   color: "#000",
   opacity: 0.3,
-  margin: 0,
-  verticalAlign: false
+  margin: 0
 };
