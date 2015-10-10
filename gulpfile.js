3ceda5230d55aeb532bee3f9ba077d52c8c660d8
Phil Cockfield <phil@cockfield.net>
2015-10-10 13:11:04 -0700

    Plumber to keep process alive.

--- gulpfile.js
+++ gulpfile.js
@@ -5,19 +5,24 @@ var eslint = require("gulp-eslint");
 var babel = require("gulp-babel");
 var SOURCE_PATH = ["./src/**/*.js", "./src/**/*.jsx"];
 
-gulp.task("lint", function() {
-  return gulp.src(SOURCE_PATH)
-             .pipe(plumber()) // Keep task alive on build errors.
-             .pipe(eslint())
-             .pipe(eslint.format());
-});
 
 gulp.task("build", function() {
   return gulp.src(SOURCE_PATH)
-    .pipe(babel({ stage: 1 }))
-    .pipe(gulp.dest("./"));
+             .pipe(plumber()) // Keep task alive on build errors.
+             .pipe(babel({ stage: 1 }))
+             .pipe(gulp.dest("./"));
 });
 
 gulp.task("prepublish", ["build"]);
 gulp.task("watch", function(callback) { gulp.watch("./src/**/*", ["build"]) });
+
+
+
+gulp.task("lint", function() {
+  return gulp.src(SOURCE_PATH)
+             .pipe(eslint())
+             .pipe(eslint.format());
+});
+
+
 gulp.task("default", ["build", "watch"]);
